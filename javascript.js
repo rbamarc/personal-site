eecda3263dd67f05d1cbf54ac03e04e8ab779155
rbamarc <rbamarc929@gmail.com>
2023-04-19 08:19:51 -0700

    worked on portfolio page

--- javascript.js
+++ javascript.js
@@ -3,3 +3,24 @@
 function showAlert(){
     alert('Thank you for your inquiry!');
 }
+
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
